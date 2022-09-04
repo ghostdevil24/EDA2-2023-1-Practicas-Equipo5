bee4c84dc0cbbb89cc2c8c37bb2286e9f5a035b2
ghostdevil24 <51523423+ghostdevil24@users.noreply.github.com>
2022-09-04 03:04:29 -0500

    Add files via upload

---
+++ Programas-Practica 1/P1-Actividad1-Ejercicio1.py
@@ -0,0 +1,46 @@
+import random
+def bubbleSort(A):
+    pasadas=0
+    for i in range(1,len(A)+1):
+        pasadas=pasadas+1
+        for j in range(len(A)-1):
+            if A[j]>A[j+1]:
+                temp=A[j]
+                A[j]=A[j+1]
+                A[j+1]=temp
+        print("pasada: ",pasadas)
+        print(A)
+        
+    return pasadas
+                
+def bubbleSort2(A):
+    bandera=True
+    pasada=0
+    while pasada<len(A)-1 and bandera:
+        bandera=False
+        for j in range(len(A)-1-pasada):
+            if(A[j]>A[j+1]):
+                bandera=True
+                temp=A[j]
+                A[j]=A[j+1]
+                A[j+1]=temp
+        pasada=pasada+1
+        print("pasada: ",pasada)
+        print(A)
+    return pasada
+def GenerarNumeros(A):
+    for i in range(6):
+            A.append(random.randrange(0,2001))
+A=[]
+GenerarNumeros(A)
+pasadas1=bubbleSort(A)
+print("\nEl arreglo de bubbleSort es:")
+print(A,"\nEl número de pasadas para bubbleSort es: ",pasadas1) 
+A=[]
+GenerarNumeros(A)
+pasadas2=bubbleSort2(A)
+print("\nEl arreglo de bubbleSort2 es:")
+print(A,"\nEl número de pasadas para bubbleSort2 es: ",pasadas2)
+
+
+

7dd8b7e6ef4c33df91535ede131ef4fe378465e5
ghostdevil24 <51523423+ghostdevil24@users.noreply.github.com>
2022-09-04 16:58:25 -0500

    Add files via upload

---
+++ Programas-Practica 1/Ejercicio4-P1-Actividad0.py
@@ -0,0 +1,20 @@
+A=[]
+Nombre=str(input("Ingresa tu nombre completo empezando por apellidos: "))
+print("Tu nombre completo:")
+print(Nombre)
+tam=len(Nombre)
+for i in range(len(Nombre)):
+    if Nombre[i]==" ":
+      A.append(i)  
+print("Nombre de pila:")
+print(Nombre[A[1]+1:tam])
+print("Localizado en los indices del %s-%s"%(A[1]+1,tam-1))
+print("Apellido Paterno:")
+print(Nombre[0:A[0]])
+print("Localizado en los indices del %s-%s"%(0,A[0]))
+print("Apellido Materno:")
+print(Nombre[A[0]+1:A[1]])
+print("Localizado en los indices del %s-%s"%(A[0]+1,A[1]))
+print("Iniciales por apellidos:")
+print("%s%s%s"%(Nombre[0],Nombre[A[0]+1],Nombre[A[1]+1]))
+print("Localizadas en los indices de %s,%s,%s"%(0,A[0]+1,A[1]+1))

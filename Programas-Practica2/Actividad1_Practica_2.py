4dadbf1cd45cc4928ee619cddbdd8ad181c6d783
ghostdevil24 <51523423+ghostdevil24@users.noreply.github.com>
2022-09-15 21:21:42 -0500

    Add files via upload

---
+++ Programas-Practica2/Actividad1_Practica_2.py
@@ -0,0 +1,26 @@
+def Intercambia(A,x,y):
+    tmp=A[x]
+    A[x]=A[y]
+    A[y]=tmp
+
+def Particionar(A,p,r):
+    print(A)
+    x=A[p]
+    print(x)
+    i=p
+    for j in range (p+1,r+1):
+        if A[j]<=x:
+            i=i+1
+            Intercambia(A, i, j)
+    Intercambia(A,i,p)
+    return i
+
+def QuickSort(A,p,r):
+    if(p<r):
+        q=Particionar(A, p, r)
+        QuickSort(A, p, q-1)
+        QuickSort(A, q+1, r)
+        
+A=[23,20,19,18,13,12,9,7,6,5,4,3,2,1,0]
+QuickSort(A, 0, len(A)-1)
+print(A)

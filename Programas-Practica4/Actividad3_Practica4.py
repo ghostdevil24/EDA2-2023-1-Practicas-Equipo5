df786cc388ad2bbd303b51b8c6dae64e13727c76
ghostdevil24 <51523423+ghostdevil24@users.noreply.github.com>
2022-10-13 02:22:50 -0500

    Add files via upload

---
+++ Programas-Practica4/Actividad3_Practica4.py
@@ -0,0 +1,19 @@
+import math
+def BusquedaBinIter(A,x,izquierda,derecha):
+  
+    while izquierda <= derecha:
+        medio=math.floor((izquierda+derecha)/2)
+        if (A[medio] == x):
+            return medio
+        elif (A[medio] < x):
+            izquierda = medio+1
+
+        else:
+                derecha=medio-1
+
+    return -1
+
+
+A=[1,10,20,30,40,50,60,70,80]
+x=80
+print(BusquedaBinIter(A,x,0,len(A)-1))

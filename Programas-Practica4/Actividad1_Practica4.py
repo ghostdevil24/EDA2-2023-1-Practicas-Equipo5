4bb6250e903e360e8419dce5c6555daedba71cdd
ghostdevil24 <51523423+ghostdevil24@users.noreply.github.com>
2022-10-13 02:18:40 -0500

    Add files via upload

---
+++ Programas-Practica4/Actividad1_Practica4.py
@@ -0,0 +1,9 @@
+def busquedaLineal(A,n,x):
+    encontrado=-1
+    for k in range(n+1):
+        if A[k] == x:
+            encontrado=k
+    return encontrado
+
+A=[1,4,6,8,9,11,15,18]
+print(busquedaLineal(A,len(A)-1,9))

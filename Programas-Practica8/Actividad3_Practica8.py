357c88adf92b56431b6618f33470b4f669c60968
Aranda Solis Ricardo <51523423+ghostdevil24@users.noreply.github.com>
2022-11-26 20:26:11 -0600

    Add files via upload

---
+++ Programas-Practica8/Actividad3_Practica8.py
@@ -0,0 +1,85 @@
+class Nodo:
+    def __init__(self, valor):
+        self.hijoIzq = None
+        self.hijoDer = None
+        self.val = valor
+
+class Arbol:
+    def __init__(self):
+        self.raiz = None
+
+    def obtenerRaiz(self):
+        return self.raiz
+
+    def agregar(self, val):
+        if(self.raiz == None):
+            self.raiz = Nodo(val)
+        else:
+            self.agregarNodo(val,self.raiz)
+
+    def agregarNodo(self,val,nodo):
+        if(val < nodo.val):
+            if (nodo.hijoIzq!=None):
+                self.agregarNodo(val,nodo.hijoIzq)
+            else:
+                nodo.hijoIzq=Nodo(val)
+
+        else:
+            if(nodo.hijoDer!=None):
+                self.agregarNodo(val,nodo.hijoDer)
+            else:
+                nodo.hijoDer=Nodo(val)
+
+    def preorden(self,nodo):
+        if(nodo!=None):
+            print(str(nodo.val))
+            if nodo.hijoIzq != None:
+                self.preorden(nodo.hijoIzq)
+            if nodo.hijoDer !=None:
+                self.preorden(nodo.hijoDer)
+
+
+    def inorden(self,nodo):
+        if(nodo!=None):
+            if nodo.hijoIzq != None:
+                self.inorden(nodo.hijoIzq)
+            print(str(nodo.val))
+            if nodo.hijoDer !=None:
+                self.inorden(nodo.hijoDer)
+
+    def postorden(self,nodo):
+        if(nodo!=None):
+            if nodo.hijoIzq != None:
+                self.postorden(nodo.hijoIzq)
+            if nodo.hijoDer !=None:
+                self.postorden(nodo.hijoDer)
+            print(str(nodo.val))
+
+    def imprimeInorden(self):
+        if(self.raiz!=None):
+            self.inorden(self.raiz)
+    
+    def imprimePreorden(self):
+        if(self.raiz!=None):
+            self.preorden(self.raiz)
+    
+    def imprimePostorden(self):
+        if(self.raiz!=None):
+            self.postorden(self.raiz)
+
+class Controladora:
+    def main(self):
+        nodos = [ 45, 7, 11, 20, 30, 3, 19, 36]
+        g=Arbol()
+
+        for i in nodos:
+            g.agregar(i)
+        print("Preorden: ")
+        g.imprimePreorden()
+        print("Inorden: ")
+        g.imprimeInorden()
+        print("Postorden: ")
+        g.imprimePostorden()
+
+obj = Controladora()
+obj.main()

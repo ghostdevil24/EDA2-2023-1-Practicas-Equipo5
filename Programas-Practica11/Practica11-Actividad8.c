775b12557707297c4c52b57c2f1225e3f6c5ed63
Isaias RM <112596331+RedoMarshallX@users.noreply.github.com>
2022-12-23 13:45:48 -0600

    Update Practica11-Actividad8.c
    Código actualizado para que concuerde con lo que puso Brenda

--- Programas-Practica11/Practica11-Actividad8.c
+++ Programas-Practica11/Practica11-Actividad8.c
@@ -2,44 +2,47 @@
 #include<stdlib.h>
 #include <math.h>
 #define n 10
+
 void llenaArreglo(int *a);
 void suma(int *a,int *b,int *c);
+
 main(){
-if(omp_in_parallel()){
-	printf("Este programa esta en paralelo\n");
+	if(omp_in_parallel()){
+		printf("Este programa esta en paralelo\n");
+	}
+	else{
+		printf("Este programa esta en serie\n");
+	}	
+	int max,*a,*b,*c;
+	a=(int *)malloc(sizeof(int)*n);
+	b=(int *)malloc(sizeof(int)*n);
+	c=(int *)malloc(sizeof(int)*n);
+	printf("A ");
+	llenaArreglo(a);
+	printf("B ");
+	llenaArreglo(b);
+	suma(a,b,c);
 }
-else{
-	printf("Este programa esta en serie\n");
-}	
-int max,*a,*b,*c;
-a=(int *)malloc(sizeof(int)*n);
-b=(int *)malloc(sizeof(int)*n);
-c=(int *)malloc(sizeof(int)*n);
-llenaArreglo(a);
-llenaArreglo(b);
-suma(a,b,c);
-}
+
 void llenaArreglo(int *a){
-int i;
-for(i=0;i<n;i++)
-{
-a[i]=rand()%n;
-printf("%d\t", a[i]);
+	int i;
+	for(i=0;i<n;i++)
+	{
+		a[i]=rand()%n;
+		printf("%d | \t", a[i]);
+	}
+	printf("\n");
 }
-printf("\n");
-}
+
 void suma(int *A, int *B, int *C){
-int i,tid;
-#pragma omp parallel private(tid)
-{
-tid = omp_get_thread_num();
-#pragma omp for
-for(i=0;i<n;i++){
-C[i]=A[i]+B[i];
-printf("hilo %d calculo C[%d]= %d\n",tid,i, C[i]);
-}
-}
-for(i=0;i<n;i++){
-printf("%d\t", C[i]);
-}
+	int i,tid;
+	#pragma omp parallel private(tid)
+	{
+		tid = omp_get_thread_num();
+		#pragma omp for
+		for(i=0;i<n;i++){
+			C[i]=A[i]+B[i];
+			printf("- hilo %d calculo C[%d]= %d\n",tid,i, C[i]);
+		}
+	}
 }
